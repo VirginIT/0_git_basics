a0a0c3ef12f22e6cb7a016e4fba86cd7a9109e54
VirginIT <stepanovmaks733@gmail.com>
2025-03-02 01:05:05 +0300

    Add factorial

--- file.py
+++ file.py
@@ -2,3 +2,14 @@
 
 lst = [i for i in range(10)]
 print(lst)
+
+def factorial(n):
+    if n < 0:
+        return "Факториал отрицательного числа не определен."
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
