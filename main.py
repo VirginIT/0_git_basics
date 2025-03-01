0cc7493dd930b9b538cdc37e25600cd64332690b
VirginIT <stepanovmaks733@gmail.com>
2025-03-02 01:06:50 +0300

    Add find_max value

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 print("Hello, world")
 print("Changed")
+
+def find_maximum(numbers):
+    if not numbers:
+        return "Список пуст."
+    maximum = numbers[0]
+    for number in numbers:
+        if number > maximum:
+            maximum = number
+    return maximum
